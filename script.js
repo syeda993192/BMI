3cc69902d10ad136e8366c2d7e7b6926cffd7492
syeda993192 <sayedafatima9919@gmail.com>
2024-10-12 16:22:19 +0500

    function BMI and function to determine BMI category

---
+++ script.js
@@ -0,0 +1,17 @@
+// Function to calculate BMI based on weight and height
+function calculateBMI(weight, height) {
+    return weight / (height * height);
+  }
+  
+  // Function to determine BMI category
+  function getBMICategory(bmi) {
+    if (bmi < 18.5) {
+      return 'Underweight';
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+      return 'Normal weight';
+    } else if (bmi >= 25 && bmi < 29.9) {
+      return 'Overweight';
+    } else {
+      return 'Obesity';
+    }
+  }
